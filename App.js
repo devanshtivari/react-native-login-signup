e7c984f88853ae924262c562e689bc77d3a7e9fe
bhanu-priyaa <devansh80771@gmail.com>
2022-10-12 02:22:45 +0530

    added style to login screen

--- App.js
+++ App.js
@@ -1,91 +1,72 @@
 import React from "react";
-import {View,Text,TouchableOpacity,StyleSheet, Dimensions, Image} from 'react-native';
-import {LinearGradient} from 'expo-linear-gradient';
-import MaterialIcons from "react-native-vector-icons/MaterialIcons"
-import * as Animatable from 'react-native-animatable';
-//MyCustomComponent = Animatable.createAnimatableComponent(MyCustomComponent);
+import { View,Text,Button,TouchableOpacity,Dimensions,StyleSheet } from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import * as Animatable from "react-native-animatable";
+import { Platform } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Animatable.Image
-          animation="bounceIn"
-          source={require("./assets/codimaths_logo.png")}
-          style={styles.logo}
-          resizeMode="stretch"
-        />
-      </View>
-      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
-        <Text style={styles.title}>Take a leap in learning!! </Text>
-        <Text style={styles.text}>Experience a new style of learning</Text>
-        <View style={styles.button}>
-          <TouchableOpacity onPress={() => {}}>
-            <LinearGradient
-              colors={["#38b6ff", "#5ce1e6"]}
-              style={styles.signIn}
-            >
-              <Text style={styles.textSign}>Get Started</Text>
-              <MaterialIcons name="navigate-next" color="#ffffff" size={20} />
-            </LinearGradient>
-          </TouchableOpacity>
-        </View>
-      </Animatable.View>
+export default function App(){
+  return(
+    <View>
+      <Text>Sign In Screen</Text>
     </View>
-  );
+  )
 }
 
-const {height} = Dimensions.get("screen");
-const height_logo = height*0.15;
-
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: "#fff"
   },
-  header: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  motto:{
-    fontSize: 25
+  header:{
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
   },
   footer: {
-    flex:1,
-    backgroundColor: "#4046af",
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    paddingVertical: 50,
-    paddingHorizontal: 30
+    flex: 3,
+    backgroundColor: "#c6d2e9",
+    borderTopLeftRadius : 30,
+    borderTopRightRadius : 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
   },
-  logo:{
-    width: height_logo*2.0,
-    height: height_logo
+  text_header: {
+    color: "#4046af",
+    fontWeight: "bold",
+    fontSize: 30
   },
-  title: {
-    color: "#c6d2e9",
-    fontSize: 30,
-    fontWeight: "bold"
+  text_footer: {
+    color: "#4046af",
+    fontSize: 18
   },
-  text:{
-    color: "#fff",
-    marginTop:5
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS == 'ios' ? 0 : -12,
+    padddingLeft: 10,
+    color: "#05375a"
   },
   button: {
-    alignItems: "flex-end",
-    marginTop: 200
+    alignItems: "center",
+    marginTop: 50
   },
   signIn: {
-    width: 150,
-    height: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 50,
-    flexDirection: "row",
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
   },
-  textSign:{
-    color: 'white',
-    fontWeight: 'bold'
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
   }
-}); 
+})
--- screens/splashscreen.js
+++ screens/splashscreen.js
@@ -1,20 +1,98 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import * as Animatable from "react-native-animatable";
+//MyCustomComponent = Animatable.createAnimatableComponent(MyCustomComponent);
 
-export default function App() {
+export default function SplashScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Animatable.Image
+          animation="bounceIn"
+          source={require("./assets/codimaths_logo.png")}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+      </View>
+      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+        <Text style={styles.title}>Take a leap in learning!! </Text>
+        <Text style={styles.text}>Experience a new style of learning</Text>
+        <View style={styles.button}>
+          <TouchableOpacity onPress={() => navigation.navigate("SignInscreen")}>
+            <LinearGradient
+              colors={["#38b6ff", "#5ce1e6"]}
+              style={styles.signIn}
+            >
+              <Text style={styles.textSign}>Get Started</Text>
+              <MaterialIcons name="navigate-next" color="#ffffff" size={20} />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
   );
 }
+
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.15;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  header: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  motto: {
+    fontSize: 25,
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#4046af",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo * 2.0,
+    height: height_logo,
+  },
+  title: {
+    color: "#c6d2e9",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "#fff",
+    marginTop: 5,
+  },
+  button: {
+    alignItems: "flex-end",
+    marginTop: 200,
+  },
+  signIn: {
+    width: 150,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    flexDirection: "row",
+  },
+  textSign: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
